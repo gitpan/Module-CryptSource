3326451d311a64daa663423f0ea7ef4f3df9cb97
☺唐鳳☻ <cpan@audreyt.org>
2002-08-17 16:14:34 +0000

    Import of AUTRIJUS/Module-CryptSource-0.02 from CPAN.
    gitpan-cpan-distribution: Module-CryptSource
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         AUTRIJUS/Module-CryptSource-0.02.tar.gz
    gitpan-cpan-author:       AUTRIJUS
    gitpan-cpan-maturity:     released

--- CryptSource.pm
+++ CryptSource.pm
@@ -1,8 +1,8 @@
 # $File: //member/autrijus/Module-CryptSource/CryptSource.pm $ $Author: autrijus $
-# $Revision: #1 $ $Change: 675 $ $DateTime: 2002/08/16 19:24:21 $
+# $Revision: #2 $ $Change: 685 $ $DateTime: 2002/08/17 08:59:09 $
 
-package CryptSource;
-$CryptSource::VERSION = '0.01';
+package Module::CryptSource;
+$Module::CryptSource::VERSION = '0.02';
 
 use strict;
 
@@ -32,25 +32,27 @@ Afterwards:
 
 One of the problems in binary packages such as perlapp or perl2exe is
 the difficulty of uncovering the source code.  While it can be argued
-that obscuring source code may be neccessary in some environments, it
-could be diasastrous when you accidentally lose the source code.
+that obscuring source code may be necessary in some environments, it
+could be disastrous when you accidentally lose the source code.
 
 This self-modifying module let you specify a key to encrypted source
 code and modules, so you can use that key to retrieve them back.
 
 To use this module, simply put C<use Module::CryptSource;> in all scripts
-and modules you wish to encrypt. Run it once with the C<--ensrc> option
+and modules you wish to encrypt.  Run it once with the C<--ensrc> option
 before running B<perlapp> or B<perl2exe>, so it can store the modified
-F<CryptSource.pm> along with your F<.exe> file; a copy of encrypted source
-code is stored in the F<CryptSource.pm> file. Run the resulting executable
-with C<--desrc> to decrypt the sources back.
+F<Module/CryptSource.pm> along with your F<.exe> file; copies of encrypted
+source files will be stored in the F<Module/CryptSource.pm> file.
+
+Run the resulting executable with C<--desrc> to decrypt the source files
+back.
 
 =head1 CAVEATS
 
 B<Storable>, B<Crypt::Rijndael> and B<Term::ReadKey> are three XS
 modules you'll have to install it yourself. It's probably possible
-to use B<Crypt::Rijndael_PP> and C<Win32::*> as the alternative,
-but I don't have the motivation yet to investigate.
+to use B<Crypt::Rijndael_PP> and C<Win32::*> as alternatives, but
+I don't have the motivation to investigate yet.
 
 If user enters a wrong decryption key, chances are that the
 C<unpack('N/a*')> will report an C<Out of memory!> error.
@@ -71,15 +73,17 @@ sub import {
 
     while (my ($file, $src) = each %Files) {
 	print "\nDecrypting $file...";
-	chmod 0644, $file; open FH, ">$file" or die $!;
+	chmod 0644, $file;
+	open FH, ">$file" or (warn("Cannot write to $file: $!"), next);
 	print FH $src;
+	close FH;
     }
 
     %Files = (); print "\n"; exit;
 }
 
-INIT {
-    return unless %Files; local *FH;
+INIT { if (%Files) {
+    local *FH;
     my $src = do { local $/; open FH, __FILE__; <FH> };
 
     $src =~ s[# <!--\n.*\n# -->]["# <!--\n".
@@ -91,16 +95,17 @@ INIT {
 	    local $/; open FH, $_; $_ => <FH>;
 	} keys %Files}));
 	$d . (' ' x (-length($d) % 16));
-    })).".\n# -->"]es; die $@ if $@;
+    })).".\n# -->"]es; die "Encryption failed: $@" if $@;
 
-    chmod 0644, __FILE__; open FH, ">".__FILE__ or die $!;
-    print FH $src; close FH;
+    chmod 0644, __FILE__;
+    open FH, ">".__FILE__ or die $!;
+    print FH $src;
+    close FH;
+
     print "\n"; exit;
-};
+} };
 
 use constant SRC => (($ARGV[0] || '') ne '--desrc') ? '' : << '.'; # <!--
-JFSyy9z2AkYiUhbDSayD1TufUlobh2bV52famlzo5gAc1ySpD9DvbUdhlg733VaseUzC1ni2OuC0
-To1FI9w2yA==
 .
 # -->
 
